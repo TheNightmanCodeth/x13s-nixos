1b0ccca4f75e7751bee8f139d40e3b99c4548543
Joe Diragi <joseph.diragi@icloud.com>
2024-09-11 18:26:42 -0400

    Initial push

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "Nix flake providing linux kernel(s) for thinkpad X13s";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    jhovold = {
+      url = "github:jhovold/linux/wip/sc8280xp-6.11-rc7";
+      flake = false;
+    };
+    gpu-fw = {
+      url = "https://download.lenovo.com/pccbbs/mobiles/n3hdr20w.exe";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, jhovold, gpu-fw, ... }: 
+    let
+      pkgs = import nixpkgs {
+        system = "aarch64-linux";
+      };
+      linux_jhovold_pkg = { version, ...}@args: pkgs.buildLinux(
+        args
+        // {
+          modDirVersion = version;
+          kernelPatches = (args.kernelPatches or [ ]) ++ [ ];
+          extraMeta.branch = nixpkgs.lib.versions.majorMinor version;
+        }
+      );
+    in
+    {
+      packages.aarch64-linux = {
+        linux-jhovold = 
+          linux_jhovold_pkg {
+            src = jhovold;
+            version = "6.11.0-rc7";
+            defconfig = "johan_defconfig";
+          };
+
+        x13s-gpu-fw = 
+          pkgs.runCommand "graphics-firmware" { } ''
+            mkdir -vp "$out/lib/firmware/qcom/sc8280xp/LENOVO/21BX"
+            ${pkgs.lib.getExe pkgs.innoextract} ${gpu-fw}
+            cp -v code\$GetExtractPath\$/*/*.mbn "$out/lib/firmware/qcom/sc8280xp/LENOVO/21BX"
+          '';
+        
+
+      };
+    };
+}
+
