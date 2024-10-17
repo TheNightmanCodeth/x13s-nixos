51977b358bd41a9f3fb1bae6dbca5be4f52a3df5
Joe Diragi <joseph.diragi@icloud.com>
2024-10-17 17:32:10 -0400

    jhovold: 6.11-rc5 -> 6.11

--- packages/default.nix
+++ packages/default.nix
@@ -17,7 +17,7 @@ in
 {
   linux_jhovold = pkgs.callPackage linux_x13s_pkg {
     src = sources.linux-jhovold;
-    version = "6.11.0-rc5";
+    version = "6.11.0";
     defconfig = "johan_defconfig";
   };
 
