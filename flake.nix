9f887d85e3b150852abbccd73a7b71bf7c96cc24
Joe Diragi <joseph.diragi@icloud.com>
2024-10-20 18:34:07 -0400

    wip

--- flake.nix
+++ flake.nix
@@ -1,136 +1,47 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
+  description = "Linux Kernel for Lenovo Thinkpad X13s";
+
+  inputs = { 
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    graphics-fw = {
+      url = "https://download.lenovo.com/pccbbs/mobiles/n3hdr20w.exe";
+      flake = false;
+    };
+
+    jhovold-src = {
+      url = "github:jhovold/linux/wip/sc8280xp-6.12-rc3";
+      flake = false;
+    };
   };
 
-  outputs =
-    inputs@{ flake-parts, self, ... }:
-    let
-      dtbName = "sc8280xp-lenovo-thinkpad-x13s.dtb";
-    in
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [ ./packages/part.nix ];
 
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-      ];
-
-      perSystem =
-        { pkgs, ... }:
-        {
-          devShells = rec {
-            default = pkgs.mkShellNoCC { packages = [ pkgs.npins ] ++ ci.nativeBuildInputs; };
-
-            ci = pkgs.mkShellNoCC {
-              packages = [
-                pkgs.cachix
-                pkgs.jq
-                pkgs.just
-                (pkgs.python3.withPackages (py: [
-                  py.PyGithub
-                  py.packaging
-                ]))
-                pkgs.pyright
-              ];
-            };
-          };
-        };
-
-      flake.nixosModules.default = import ./module.nix { inherit dtbName; };
-
-      flake.nixosConfigurations = {
-        example = inputs.nixpkgs.lib.nixosSystem {
-          system = "aarch64-linux";
-          modules = [
-            self.nixosModules.default
-            (
-              { config, pkgs, ... }:
-              {
-                nixos-x13s.enable = true;
-                nixos-x13s.kernel = "jhovold"; # jhovold is default, but mainline supported
-
-                # allow unfree firmware
-                nixpkgs.config.allowUnfree = true;
-
-                # define your fileSystems
-                fileSystems."/".device = "/dev/notreal";
-              }
-            )
-          ];
-        };
-
-        iso = inputs.nixpkgs.lib.nixosSystem {
-          system = "aarch64-linux";
-          modules = [
-
-            self.nixosModules.default
-            (
-              {
-                modulesPath,
-                config,
-                lib,
-                pkgs,
-                ...
-              }:
-              let
-                image = import "${inputs.nixpkgs}/nixos/lib/make-disk-image.nix" {
-                  inherit config lib pkgs;
-
-                  name = "nixos-x13s-bootstrap";
-                  diskSize = "auto";
-                  format = "raw";
-                  partitionTableType = "efi";
-                  copyChannel = false;
-                };
-
-              in
-              {
-                hardware.deviceTree = {
-                  enable = true;
-                  name = "qcom/${dtbName}";
-                };
-
-                system.build.bootstrap-image = image;
-
-                boot = {
-                  initrd = {
-                    systemd.enable = true;
-                    systemd.emergencyAccess = true;
-                  };
-
-                  loader = {
-                    grub.enable = false;
-                    systemd-boot.enable = true;
-                    systemd-boot.graceful = true;
-                  };
-                };
-
-                nixpkgs.config.allowUnfree = true;
-
-                nixos-x13s = {
-                  enable = true;
-                  kernel = "jhovold";
-                  bluetoothMac = "02:68:b3:29:da:98";
-                  wifiMac = "F4:A8:0D:FF:7C:87";
-                };
-
-                fileSystems = {
-                  "/boot" = {
-                    fsType = "vfat";
-                    device = "/dev/disk/by-label/ESP_nixinstaller";
-                  };
-                  "/" = {
-                    device = "/dev/disk/by-label/nixos_nixinstaller";
-                    fsType = "ext4";
-                    autoResize = true;
-                  };
-                };
-              }
-            )
-          ];
-        };
-      };
+  outputs = { nixpkgs, jhovold-src, graphics-fw, ... }:
+  let
+    version = "6.12.0-rc3";
+    pkgs = import nixpkgs {
+      system = "aarch64-linux";
     };
+  in 
+  {
+    boot.kernelPackages = pkgs.linuxPackages.extend (pkgs.lib.const (super: {
+      kernel = super.kernel.overrideDerivation (drv: {
+        nativeBuildInputs = (drv.nativeBuildInputs or []) ++ [ pkgs.zlib pkgs.lz4 ];
+      });
+    }));
+
+    graphics-firmware = pkgs.runCommand "graphics-firmware" { } ''
+      mkdir -vp "$out/lib/firmware/qcom/sc8280xp/LENOVO/21BX"
+      ${pkgs.lib.getExe pkgs.innoextract} ${graphics-fw}
+      cp -v code\$GetExtractPath\$/*/*.mbn "$out/lib/firmware/qcom/sc8280xp/LENOVO/21BX/"
+    '';
+
+    packages.aarch64-linux.jhovold-linux = pkgs.buildLinux {
+      inherit version;
+      src = jhovold-src;
+      defconfig = "johan_defconfig";
+      modDirVersion = version;
+      extraMeta.branch = "wip/sc8280xp-${version}";
+    };
+  };
 }
